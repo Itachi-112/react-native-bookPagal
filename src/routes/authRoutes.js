feaa789ffc882d2dfae1fa2bca982394a5c4b2f4
Itachi-112 <botg5744@gmail.com>
2025-04-01 01:07:38 +0530

    adding created at in auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -58,6 +58,7 @@ router.post("/register", async (req, res) => {
             username: user.username,
             email: user.email,
             profileImage: user.profileImage,
+            createdAt: user.createdAt
          },
       });
 
@@ -91,7 +92,8 @@ router.post("/login", async (req, res) => {
             id: user._id,
             username: user.username,
             email: user.email,
-            profileImage: user.profileImage
+            profileImage: user.profileImage,
+            createdAt: user.createdAt
          }
       });
    }
